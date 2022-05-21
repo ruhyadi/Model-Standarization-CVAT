fd3ad61247e0e0e745c94ae72f25cedb3e13f46f
ruhyadi <ruhyadi.dr@gmail.com>
2022-05-21 02:48:42 +0000

    fixing utils

--- streamlit/gui.py
+++ streamlit/gui.py
@@ -1,24 +1,46 @@
 
+from io import StringIO
 import os
 import streamlit as st
 import git
-from utils import clone_repo
+from utils import clone_repo, create_release, upload_assets
 
 def main():
 
     st.title('Model Registry')
 
-    col01, col02 = st.columns([3, 1])
+    st.subheader('Repository')
+    col01, col02, col03 = st.columns([3, 1, 1])
     with col01:
-        repo_url = st.text_input('Repository (*):', 'https://github.com/ruhyadi/ruhyadi')
+        repo_url = st.text_input('Repository (*):', 'https://github.com/ruhyadi/sample-release')
     with col02:
-        tag = st.text_input('Branch (*):', 'v1.0')
-    clone_btn = st.button('Clone')
+        token = st.text_input('Token (*):', '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')
+    with col03:
+        branch = st.text_input('Branch (*):', 'main')
     
-    if clone_btn:
+    st.subheader('Releases')
+    col11, col12, col13 = st.columns([2, 2, 1])
+    with col11:
+        release_title = st.text_input('Release title: (*)', 'v1.0 release')
+    with col12:
+        release_desc = st.text_input('Release description: (*)', 'Release assets v1.0')
+    with col13:
+        release_tag = st.text_input('Release Tag (*):', 'v1.0')
+
+    assets = st.file_uploader('Model Asset(s) (*):', accept_multiple_files=True)
+
+    assets_btn = st.button('Upload Assets')
+
+    if assets_btn:
         clone_repo(repo_url)
         st.success(f'Success Clone Repository {repo_url.split("/")[3:5]}')
-    
-    
+
+        upload_url = create_release(release_title, release_tag, branch, release_desc, repo_url, token)
+        st.success(f'Success Release {release_tag}')
+
+        for asset in assets:
+            filename = asset.name
+            upload_assets(filename, upload_url, token)
+
 if __name__ == '__main__':
     main()
--- streamlit/utils.py
+++ streamlit/utils.py
@@ -1,10 +1,10 @@
 
 from pathlib import Path
 import shutil
-from unicodedata import name
-from wsgiref import headers
 import git
 import os
+import json
+import requests
 
 def clone_repo(url, force=True):
     # https://github.com/ruhyadi/Model-Standarization-CVAT
@@ -22,13 +22,16 @@ def clone_repo(url, force=True):
     except:
         print('[INFO] Repository already exist')
 
-def create_release():
+def create_release(title, tag, branch, desc, git_url, token):
+
+    username = git_url.split('/')[3]
+    reponame = git_url.split('/')[4]
 
     dict_ = {
-        "tag_name":"v1.3",
-        "target_commitish":"main",
-        "name":"v1.3",
-        "body":"Sample Release",
+        "tag_name":f"{tag}",
+        "target_commitish":f"{branch}",
+        "name":f"{title}",
+        "body":f"{desc}",
         "draft":False,
         "prerelease":False,
         "generate_release_notes":False
@@ -36,75 +39,90 @@ def create_release():
 
     headers = {
         "Accept": "application/vnd.github.v3+json",
-        "Authorization": "token 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",
-        "Content-Type": "application/zip"
+        "Authorization": f"token {token}",
     }
 
     release_json = json.dumps(dict_)
 
     release = requests.post(
-        'https://api.github.com/repos/ruhyadi/sample-release/releases',
+        f'https://api.github.com/repos/{username}/{reponame}/releases',
         headers=headers,
         data=release_json
         )
 
-def upload_assets():
-    
-    assets_dict = {
-        "name" : "sample003.zip",
-        "label" : "sample003"
-    }
+    upload_url = release.json()['upload_url']
 
-    assets = requests.post(
-        'https://uploads.github.com/repos/ruhyadi/sample-release/releases/67404165/assets?name=sample003.zip',
-        headers=headers,
-        data=json.dumps(assets_dict)
-    )
+    return upload_url
 
+def upload_assets(filename, upload_url, token):
 
-if __name__ == '__main__':
-
-    import requests
-    import json
-
-    dict_ = {
-        "tag_name":"v1.3",
-        "target_commitish":"main",
-        "name":"v1.3",
-        "body":"Sample Release",
-        "draft":False,
-        "prerelease":False,
-        "generate_release_notes":False
-    }
+    upload_url_ = upload_url.split('{')[0]
 
     headers = {
         "Accept": "application/vnd.github.v3+json",
-        "Authorization": "token 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",
-        "Content-Type": "application/zip"
+        "Authorization": f"token {token}",
+        "Content-Type": "image/png"
+        # "Content-Type": "application/zip"
     }
-
-    # release_json = json.dumps(dict_)
-
-    # release = requests.post(
-    #     'https://api.github.com/repos/ruhyadi/sample-release/releases',
-    #     headers=headers,
-    #     data=release_json
-    #     )
-
-    # print(release)
-    # print(release.json())
 
     assets_dict = {
-        "name" : "sample003.zip",
-        "label" : "sample003"
+        "name" : filename,
+        "label" : filename
     }
-
-    assets = requests.post(
-        'https://uploads.github.com/repos/ruhyadi/sample-release/releases/67404165/assets?name=sample003.zip',
+    # upload_url_ = 'https://uploads.github.com/repos/ruhyadi/sample-release/releases/66158665/assets'
+    response_assets = requests.post(
+        f'{upload_url_}?name={filename}',
         headers=headers,
         data=json.dumps(assets_dict)
     )
 
-    # print(release.json())
-    print(assets)
-    print(assets.json())
+    print(response_assets)
+    print(response_assets.json())
+
+if __name__ == '__main__':
+    pass
+
+    # import requests
+    # import json
+
+    # dict_ = {
+    #     "tag_name":"v1.3",
+    #     "target_commitish":"main",
+    #     "name":"v1.3",
+    #     "body":"Sample Release",
+    #     "draft":False,
+    #     "prerelease":False,
+    #     "generate_release_notes":False
+    # }
+
+    # headers = {
+    #     "Accept": "application/vnd.github.v3+json",
+    #     "Authorization": "token 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",
+    #     "Content-Type": "application/zip"
+    # }
+
+    # # release_json = json.dumps(dict_)
+
+    # # release = requests.post(
+    # #     'https://api.github.com/repos/ruhyadi/sample-release/releases',
+    # #     headers=headers,
+    # #     data=release_json
+    # #     )
+
+    # # print(release)
+    # # print(release.json())
+
+    # assets_dict = {
+    #     "name" : "sample003.zip",
+    #     "label" : "sample003"
+    # }
+
+    # assets = requests.post(
+    #     'https://uploads.github.com/repos/ruhyadi/sample-release/releases/67404165/assets?name=sample003.zip',
+    #     headers=headers,
+    #     data=json.dumps(assets_dict)
+    # )
+
+    # # print(release.json())
+    # print(assets)
+    # print(assets.json())
